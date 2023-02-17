299d542877b4fd16f91e92a2f4cc13f013185184
Aleksandr <trololo1009@gmail.com>
2023-02-17 14:35:55 +0500

    added Title UI comp && main theme fot it

---
+++ src/UI/Title/Title.tsx
@@ -0,0 +1,21 @@
+import { FC, HTMLProps, ReactNode } from "react";
+import classNames from "classnames/bind";
+import cls from "./Title.module.scss";
+
+const cn = classNames.bind(cls);
+
+export const enum ThemeTitle {
+  MAIN = "main",
+}
+
+interface TitleProps extends HTMLProps<HTMLHeadingElement> {
+  className?: string;
+  children: ReactNode;
+  theme?: string;
+}
+
+export const Title: FC<TitleProps> = (props) => {
+  const { className, children, theme = ThemeTitle.MAIN } = props;
+
+  return <h2 className={cn(cls.Title, cls[theme], className)}>{children}</h2>;
+};
