81f2e07aba7fcbdac86fb3fb3011fbfa0ea387ac
master <seytkhanbalikbaev@gmail.com>
2023-02-18 18:34:39 +0600

    modified validation

--- src/helpers/validation.ts
+++ src/helpers/validation.ts
@@ -1,6 +1,17 @@
 import * as Yup from 'yup';
 
-export const authorizationSchema = Yup.object().shape({
+export const registerSchema = Yup.object().shape({
   name: Yup.string().required('Обязательное поле'),
   phoneNumber: Yup.string().required('Обязательное поле'),
+});
+
+export const getSmsCodeSchema = Yup.object().shape({
+  confirmSmsCode: Yup.string().required('Обязательное поле'),
+  password: Yup.string().required('Обязательное поле'),
+  confirmPassword: Yup.string().required('Обязательное поле'),
+});
+
+export const loginSchema = Yup.object().shape({
+  phoneNumberLogin: Yup.string().required('Обязательное поле'),
+  passwordLogin: Yup.string().required('Обязательное поле'),
 });
