2a618a6a6d5b622e4982b6aef1b4976d13144ad7
master <seytkhanbalikbaev@gmail.com>
2023-04-19 09:12:40 +0600

    corrected next config and filter section

--- next.config.js
+++ next.config.js
@@ -1,17 +1,25 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ["kazkabel-back.zoom-app.kz"],
+    domains: ['api.cable.kz', 'kazkabel-back.zoom-app.kz'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.cable.kz',
+        port: '',
+        pathname: '/account123/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'kazkabel-back.zoom-app.kz',
+        port: '',
+        pathname: '/account123/**',
+      },
+    ],
   },
-  remotePatterns: [
-    {
-      protocol: "https",
-      hostname: "kazkabel-back.zoom-app.kz",
-      port: "",
-      pathname: "/account123/**",
-    },
-  ],
 };
 
 module.exports = nextConfig;
